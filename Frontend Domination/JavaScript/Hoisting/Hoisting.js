33f52ea1455bea0e46deae6cde1eb9a67a5009c8
Hpshiva <shivakumar.bfgi@gmail.com>
2024-06-07 01:34:51 +0530

    Day-03

---
+++ Frontend Domination/JavaScript/Hoisting/Hoisting.js
@@ -0,0 +1,15 @@
+// variable ko access krna before creating the variable declared and initilization
+//OR
+// variable and functions are hoisted which means there declaration is moved on the top of code
+
+//UNDEFINED
+console.log(a); //undefined
+let a = 4;
+
+// {what it will do it will take like let a=4; as let a; which goes to on the top where interpreter starts that why its show undefined}
+//
+//
+//
+// NOT-DEFINED
+console.log(b); //NOT-DEFINED
+// { why NOT-DEFINED then     -    when it is not even declared }
